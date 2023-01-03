9b87611af875948fd5cd94a194030bfbea6a29aa
awgore99 <awgore99@gmail.com>
2023-01-02 19:37:05 -0600

    Added highscore functionality

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# ChallengeFour
-An interactive quiz about JavaScript that stores highscores
+# JavaScript Quiz - Challenge Four
+
+A timed javascript quiz that takes in input from the user, and updates the acores or timer accordingly.
+
+## Installation
+
+N/A
+
+## Usage
+
+After the user hits the start quiz button, they will be prompted with questions about javascript that will then either add to their score, or subtract from the timer depending on whether the answer was correct or not. HighScores are saved at the end.
+
+## Credits
+
+N/A
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- script.js
+++ script.js
@@ -5,9 +5,11 @@ let questionNumber = 0;
 let questionEl = document.getElementById("questions");
 let answerEl = document.getElementById('answers');
 let answer = "";
-let scoreEl = document.getElementById("points")
+let scoreEl = document.getElementById("points");
 let score = 0;
-
+let sumbitButton = document.getElementById("nameButton");
+let playerName = document.getElementById("playerName");
+let scores = document.getElementById("scores");
 //Questions Object
 const questions = [
     {
@@ -28,9 +30,10 @@ const questions = [
 ];
 
 //Hides the quiz and finish containers until needed
-document.getElementById("quiz").classList.add("d-none");
-document.getElementById("gameEnd").classList.add("d-none");
-
+if (document.getElementById("quiz") && document.getElementById("gameEnd")) {
+    document.getElementById("quiz").classList.add("d-none");
+    document.getElementById("gameEnd").classList.add("d-none");
+};
 function start() {
     //Hiding instructions container and showing quiz container
     document.getElementById("instructions").classList.add("d-none");
@@ -49,37 +52,79 @@ function setTimer() {
         if (timerCount === 0) {
             clearInterval(timer);
             endGame();
+        } else if (questionNumber === questions.length + 1) {
+            clearInterval(timer);
+            endGame();
         }
     }, 1000)
 };
 
-function getQuestions(){
+function getQuestions() {
     //Save question answer globally
-    answer=questions[questionNumber].answer;
+    answer = questions[questionNumber].answer;
 
     questionEl.textContent = questions[questionNumber].question;
-    answerEl.innerHTML="";
+    answerEl.innerHTML = "";
     //Iterate through choices array in the given question
     let choices = questions[questionNumber].choices;
-    for (var i=0; i<choices.length; i++) {
+    for (var i = 0; i < choices.length; i++) {
         //Create clickability for each choice, as well as adding them to the page
         var selection = document.createElement('button');
         selection.textContent = choices[i];
         answerButton = answerEl.appendChild(selection);
     }
+    questionNumber++;
 }
 
-startButton.addEventListener("click", start);
+if (startButton) {
+    startButton.addEventListener("click", start);
+};
 //Checks the answer and then either updates the score, or timer
-answerEl.addEventListener("click", function(event){
-    let correctBool = document.getElementById("correctBool");
-    if(answer === event.target.textContent){
-        correctBool.innerHTML = "Correct! 1 point added to your score!";
-        score++;
-        scoreEl.textContent = "Score: " + score;
-    } else {
-        correctBool.innerHTML = "Incorrect! 3 seconds removed from your quiz timer!"
-        timerCount = timerCount - 3;
+if (answerEl) {
+    answerEl.addEventListener("click", function (event) {
+        let correctBool = document.getElementById("correctBool");
+        if (answer === event.target.textContent) {
+            correctBool.innerHTML = "Correct! 1 point added to your score!";
+            score++;
+            scoreEl.textContent = "Score: " + score;
+        } else {
+            correctBool.innerHTML = "Incorrect! 3 seconds removed from your quiz timer!"
+            timerCount = timerCount - 3;
+        }
+        getQuestions();
+    })
+};
+
+function endGame() {
+    document.getElementById("quiz").classList.add("d-none");
+    document.getElementById("gameEnd").classList.remove("d-none");
+}
+
+function saveScore() {
+    let name = playerName.value.trim();
+    let scores = JSON.parse(window.localStorage.getItem('scores')) || [];
+    let newScore = {
+        score: score,
+        name: name
     }
-    getQuestions();
-})
+    scores.push(newScore);
+    window.localStorage.setItem('scores', JSON.stringify(scores));
+    window.location.href = 'scores.html';
+}
+
+function getScores() {
+    let scores = JSON.parse(window.localStorage.getItem('scores'));
+    for (let i = 0; i < scores.length; i++) {
+        var li = document.createElement('li');
+        li.textContent = scores[i].name + ' - ' + scores[i].score;
+
+        var scoreBox = document.getElementById('scores');
+        scoreBox.appendChild(li);
+    }
+};
+if (sumbitButton) {
+    sumbitButton.addEventListener("click", saveScore);
+};
+if (scores) {
+    getScores();
+};
